c6a05de7d037e62338a72b89f01473eb035aeb05
Inerial <rlxo132@naver.com>
2020-08-29 09:57:06 +0900

    1

--- 125.py
+++ 125.py
@@ -5,3 +5,17 @@ class Solution:
             if c.isalnum():
                 strs.append(c.lower())
         return strs == list(reversed(strs))
+
+from collections import deque
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = deque()
+        for c in s:
+            if c.isalnum():
+                strs.append(c.lower())
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+        return True
+
+
