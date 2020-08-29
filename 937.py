d70d3dac72443720c115eb0d61389870cc470bd8
Inerial <rlxo132@naver.com>
2020-08-29 14:30:36 +0900

    1

--- 937.py
+++ 937.py
@@ -8,18 +8,16 @@ class Solution:
                 let.append(str)
             else:
                 dig.append(str)
-        for i in range(len(let)-1):
-            for j in range(i , len(let)):
-                left = let[i].split(' ',maxsplit=1)
-                right = let[j].split(' ',maxsplit=1)
-                if left[1] > right[1] :
-                    tmp = let[i]
-                    let[i] = let[j]
-                    let[j] = tmp
-                if left[1] == right[1]:
-                    if left[0] > right[0]:
-                        tmp = let[i]
-                        let[i] = let[j]
-                        let[j] = tmp
+                
+        let.sort(key=lambda x: (x.split(" ", maxsplit=1)[1], x.split(" ", maxsplit=1)[0]))
+        
+        # for i in range(len(let)-1):
+        #     for j in range(i , len(let)):
+        #         left = let[i].split(' ',maxsplit=1)
+        #         right = let[j].split(' ',maxsplit=1)
+        #         if left[1] > right[1] :
+        #             let[i], let[j] = let[j], let[i]
+        #         elif left[1] == right[1]:
+        #             if left[0] > right[0]:
+        #                 let[i], let[j] = let[j], let[i]
         return let + dig
-
