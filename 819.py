2e7fd5a2f48146c05d663dba233f7fd12679f366
Inerial <rlxo132@naver.com>
2020-08-29 15:43:37 +0900

    1

--- 819.py
+++ 819.py
@@ -22,3 +22,20 @@ class Solution:
                 bestnum = n
                 
         return beststring
+
+import collections
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        output = {}
+        beststring = ''
+        paragraph = list(paragraph.lower())
+        for i in range(len(paragraph)):
+            if not paragraph[i].isalpha():
+                paragraph[i] = ' '
+        paragraph = ''.join(paragraph)
+        
+        for s, n in collections.Counter(paragraph.split()).most_common():
+            if s not in banned:
+                return s
+        
+        return beststring
