40d8e41317aa61f087916a89ad78e881f0229a28
Inerial <rlxo132@naver.com>
2020-08-29 11:10:22 +0900

    1

---
+++ 819.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        output = {}
+        bestnum = 0
+        beststring = ''
+        paragraph = list(paragraph.lower())
+        for i in range(len(paragraph)):
+            if not paragraph[i].isalpha():
+                paragraph[i] = ' '
+        paragraph = ''.join(paragraph)
+        
+        for strs in paragraph.split():
+            if strs in output:
+                output[strs] += 1
+            else:
+                output[strs] = 1
+        for s, n in output.items():
+            if s in banned:
+                continue
+            if bestnum < n:
+                beststring = s
+                bestnum = n
+                
+        return beststring
